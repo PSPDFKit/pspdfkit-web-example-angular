7aef4bf0d8954721e87e2190080fcb94dc49cfa1
PSPDFKit <support@pspdfkit.com>
2021-05-27 11:57:57 +0100

    PSPDFKit for Web Angular Example

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+import PSPDFKit from "pspdfkit";
+import licenseKey from "../license-key";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["app.component.css"],
+})
+export class AppComponent {
+  title = "PSPDFKit for Web Angular Example";
+
+  ngAfterViewInit(): void {
+    PSPDFKit.load({
+      baseUrl: location.protocol + "//" + location.host + "/assets/",
+      document: "/assets/example.pdf",
+      container: ".pspdfkit-container",
+      licenseKey,
+    }).then((instance) => {
+      // For the sake of this demo, store the PSPDFKit for Web instance
+      // on the global object so that you can open the dev tools and
+      // play with the PSPDFKit API.
+
+      // @ts-expect-error
+      window.instance = instance;
+    });
+  }
+}
