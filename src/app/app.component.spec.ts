aca029e656aed6eba1852ba9eced8a2651adbdea
PSPDFKit <support@nutrient.io>
2025-02-07 11:25:18 +0100

    Nutrient Web SDK Angular Example

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    expect(app).toBeTruthy();
+  });
+
+  it('should have as title "Nutrient Web SDK Angular Example"', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    expect(app.title).toEqual("Nutrient Web SDK Angular Example");
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector(".toolbar")?.textContent).toContain(
+      " PSPDFKit Angular Application"
+    );
+  });
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import PSPDFKit from 'pspdfkit';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Nutrient Web SDK Angular Example';
+
+  ngAfterViewInit() {
+    PSPDFKit.load({
+      // Use the assets directory URL as a base URL. PSPDFKit will download its library assets from here.
+      baseUrl: location.protocol + '//' + location.host + '/assets/',
+      document: '/assets/example.pdf',
+      container: '#pspdfkit-container',
+    }).then((instance) => {
+      // For the sake of this demo, store the Nutrient Web SDK instance
+      // on the global object so that you can open the dev tools and
+      // play with the PSPDFKit API.
+      (window as any).instance = instance;
+    });
+  }
+}
